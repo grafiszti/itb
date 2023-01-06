65d390e199ab1e223ef42772ed59371382cd8717
grafiszti <mateusz.choinski100@gmail.com>
2023-01-06 12:39:48 +0100

    added new example image, added merging example to example basics notebook

--- src/itb/consts.py
+++ src/itb/consts.py
@@ -5,3 +5,4 @@ ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 TEST_IMGS_DIR = os.path.join(ROOT_DIR, "test_data")
 TEST_IMG_1 = os.path.join(TEST_IMGS_DIR, "img1.jpg")
 TEST_IMG_2 = os.path.join(TEST_IMGS_DIR, "img1.JPG")
+TEST_IMG_3 = os.path.join(TEST_IMGS_DIR, "img2.jpg")
